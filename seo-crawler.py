224aeaca924a8f848a41fc98da3bac8001432314
Uğur Kubilay ÇAM <32200607+ndorte@users.noreply.github.com>
2019-02-12 17:13:06 +0300

    Update seo-crawler.py

--- seo-crawler.py
+++ seo-crawler.py
@@ -6,7 +6,8 @@ taranacaklar = list()
 dortYuzDort = list()
 disLinkler = list()
 
-header = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
 url = ""
 linkuzunlugu = (len(url))
@@ -19,8 +20,12 @@ def ilksayfa(url):
         if link.has_attr("href"):
             if link["href"][:linkuzunlugu] == url:
                 taranacaklar.append(link["href"])
+            elif link["href"] == "/" or link["href"] == "#" or link["href"][:7] == "mailto:" or "#" in link["href"]:
+                continue
             elif link["href"][:linkuzunlugu+1] != "/" and link["href"][:4] == "http":
                 disLinkler.append(link["href"])
+            elif link["href"][0] == "/":
+                taranacaklar.append(url+link["href"])
             elif link["href"][:linkuzunlugu+1] != "/":
                 taranacaklar.append(url+"/"+link["href"])
             else:
@@ -36,53 +41,64 @@ for tara in taranacaklar:
     if tara not in tarananlar:
         print("Sayfa Taranıyor: " + tara)
         cek = get(tara, headers=header)
-        soup = BeautifulSoup(cek.text, "html.parser")
-        linkler = soup.findAll('a')
-        for link in linkler:
-            if link.has_attr("href"):
-                if link["href"][:linkuzunlugu] == url:
-                    if link["href"] in taranacaklar:
-                        continue
-                    else:
-                        if link["href"][-4:] == ".jpg" or link["href"][-4:] == ".png" or link["href"][-4:] == ".pdf":
+        if cek.status_code == 200:
+            soup = BeautifulSoup(cek.text, "html.parser")
+            linkler = soup.findAll('a')
+            for link in linkler:
+                if link.has_attr("href"):
+                    if link["href"][:linkuzunlugu] == url:
+                        if link["href"] in taranacaklar:
                             continue
                         else:
-                            taranacaklar.append(link["href"])
-                elif "//" in link["href"] or "///" in link["href"]:
-                    continue
-                elif link["href"] == "/":
-                    continue
-                elif link["href"]== "#" or link["href"][:8] == "#respond":
-                    continue
-                elif link["href"][:7] == "mailto:" or link["href"][:4] == "tel:":
-                    continue
-                elif link["href"][:linkuzunlugu + 1] != "/" and link["href"][:4] == "http":
-                    if link["href"][:30] == "https://www.facebook.com/share" or link["href"][:30] == "https://web.whatsapp.com/send?" or link["href"][:30] == "http://www.facebook.com/sharer" or link["href"][:30] == "https://twitter.com/share?text" or link["href"][:30] == "https://twitter.com/intent/twe" or link["href"][:30] == "http://www.linkedin.com/shareA" or link["href"][:30] == "http://reddit.com/submit?url=h" or link["href"][:30] == "http://vk.com/share.php?url=ht" or link["href"][:30] == "http://www.tumblr.com/share/li" or link["href"][:30] == "http://linkedin.com/shareArtic" or link["href"][:30] == "http://pinterest.com/pin/creat" or link["href"][:30] == "https://plus.google.com/share?":
+                            if link["href"][-4:] == ".jpg" or link["href"][-4:] == ".png" or link["href"][-4:] == ".pdf":
+                                continue
+                            else:
+                                taranacaklar.append(link["href"])
+                    elif link["href"] == "/" or link["href"] == "#" or link["href"][:7] == "mailto:":
                         continue
-                    else:
-                        if link["href"] in disLinkler:
+                    elif link["href"][:linkuzunlugu + 1] != "/" and link["href"][:4] == "http":
+                        if link["href"][:30] == "https://www.facebook.com/share" \
+                                or link["href"][:30] == "http://www.facebook.com/sharer" \
+                                or link["href"][:30] == "https://twitter.com/share?text" \
+                                or link["href"][:30] == "https://twitter.com/intent/twe" \
+                                or link["href"][:30] == "http://www.linkedin.com/shareA" \
+                                or link["href"][:30] == "http://reddit.com/submit?url=h" \
+                                or link["href"][:30] == "http://vk.com/share.php?url=ht" \
+                                or link["href"][:30] == "http://www.tumblr.com/share/li" \
+                                or link["href"][:30] == "https://e-rapor.prosafety.com." \
+                                or link["href"][:30] == "http://linkedin.com/shareArtic" \
+                                or link["href"][:30] == "http://pinterest.com/pin/creat" \
+                                or link["href"][:30] == "https://plus.google.com/share?":
                             continue
                         else:
-                            disLinkler.append(link["href"])
+                            disLinkler.append("Dış link: " + link["href"] + " Bulunan Sayfa: "+ tara)
+                            print(disLinkler)
                             print("Dış link: " + link["href"] + " Bulunan Sayfa: "+ tara)
-                            with open("dislink.txt", "a", encoding="utf-8") as file:
-                                file.writelines(link["href"] + " Bulunan Sayfa: "+ tara +"\n")
+                            with open("dreamdislink.txt", "a", encoding="utf-8") as file:
+                                file.writelines(" Bulunan Sayfa: "+ tara + " | " +link["href"] + "\n")
                                 file.close()
-                elif link["href"][:linkuzunlugu + 1] != "/":
-                    if url + "/" + link["href"] in taranacaklar:
-                        continue
+                    elif link["href"][0] == "/":
+                        taranacaklar.append(url + link["href"])
+                    elif link["href"][:linkuzunlugu + 1] != "/":
+                        if url + "/" + link["href"] in taranacaklar:
+                            continue
+                        else:
+                            taranacaklar.append(url + "/" + link["href"])
                     else:
-                        taranacaklar.append(url + "/" + link["href"])
+                        print("Ne oldugu tespit edilemedi. Lütfen manuel kontrol edin: " + link["href"])
                 else:
-                    print("Ne oldugu tespit edilemedi. Lütfen manuel kontrol edin: " + link["href"])
                     continue
-            else:
-                continue
+        else:
+            dortYuzDort.append(("["+str(cek.status_code)+"] " + tara))
         tarananlar.append(tara)
     elif tara in tarananlar:
         continue
 
-with open("taranansayfalar.txt", "a", encoding="utf-8") as fil:
+with open("2.txt", "w", encoding="utf-8") as fil:
     for i in tarananlar:
         fil.writelines(i + "\n")
     fil.close()
+
+print("404 Veren Sayfalar")
+for i in dortYuzDort:
+    print(i)
